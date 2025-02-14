1f8c2b103ff7d5ca4115bffc87b8a8dd13075c6a
Adrien <adrienleyval4@gmail.com>
2025-02-14 14:43:02 +0100

    build: add database url

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -3,14 +3,15 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 export const db = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: parseInt(process.env.DB_PORT!),
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_NAME,
+    // port: parseInt(process.env.DB_PORT!),
+    // max: 20,
+    // idleTimeoutMillis: 30000,
+    // connectionTimeoutMillis: 2000,
+    connectionString: process.env.DATABASE_URL
 });
 
 export const connectDB = async (): Promise<void> => {
